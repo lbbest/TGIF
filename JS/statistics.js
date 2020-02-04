c24130b9f691313e61c90adf2ddf38ca7b7b2132
Liam Best <liambbest@googlemail.com>
2020-02-04 13:31:19 +0100

    Senate Attendance Page Built

--- JS/statistics.js
+++ JS/statistics.js
@@ -25,6 +25,18 @@ buildPartyArr(houseData.results[0].members, "D", houseDemocrats);
 buildPartyArr(houseData.results[0].members, "R", houseRepublicans);
 buildPartyArr(houseData.results[0].members, "I", houseIndependents);
 
+// Build arrays containing all members of particular chamber
+
+let allSenateMembers = Array.prototype.concat.apply(
+  [],
+  [senateDemocrats, senateRepublicans, senateIndependents]
+);
+
+let allHouseMembers = Array.prototype.concat.apply(
+  [],
+  [houseDemocrats, houseRepublicans, houseIndependents]
+);
+
 // Initialise statistics object
 
 let statistics = {
@@ -59,8 +71,13 @@ document.querySelector("#senate-democrat-member-count").textContent =
   statistics.Number_of_Democrats_Senate;
 document.querySelector("#senate-independent-member-count").textContent =
   statistics.Number_of_Independents_Senate;
+document.querySelector("#senate-member-count").textContent =
+  statistics.Number_of_Independents_Senate +
+  statistics.Number_of_Democrats_Senate +
+  statistics.Number_of_Republicans_Senate;
 
 // Calculate average vote with percentage
+
 function avgVoteWith(dataSource) {
   let total = 0;
   let average = 0;
@@ -89,8 +106,6 @@ statistics.Pct_Voted_With_Party_Independents_House =
 
 // Insert senate % voted with data into at a glance table
 
-console.log(statistics);
-
 document.querySelector(
   "#senate-republican-voted-with"
 ).textContent = statistics.Pct_Voted_With_Party_Republicans_Senate.toFixed(2);
@@ -100,3 +115,73 @@ document.querySelector(
 document.querySelector(
   "#senate-independent-voted-with"
 ).textContent = statistics.Pct_Voted_With_Party_Independents_Senate.toFixed(2);
+document.querySelector("#senate-voted-with").textContent = (
+  (statistics.Pct_Voted_With_Party_Independents_Senate +
+    statistics.Pct_Voted_With_Party_Democrats_Senate +
+    statistics.Pct_Voted_With_Party_Republicans_Senate) /
+  3
+).toFixed(2);
+
+// Calculate least engaged (Bottom 10% Attendance) and build table
+
+function descTableTenPctAtt(arr, table) {
+  arr.sort(function(obj1, obj2) {
+    return obj2.missed_votes_pct - obj1.missed_votes_pct;
+  });
+  for (i = 0; i < arr.length * 0.1; i++) {
+    let newRow = table.insertRow();
+    let newCell = newRow.insertCell();
+    let firstName = arr[i].first_name;
+    let middleName = arr[i].middle_name;
+    let lastName = arr[i].last_name;
+    if (middleName != null) {
+      fullName = firstName + " " + middleName + " " + lastName;
+    } else {
+      fullName = firstName + " " + lastName;
+    }
+    newCell.innerHTML = `<a href="${arr[i].url}" target="_blank">${fullName}</a>`;
+    newLink = newCell = newRow.insertCell();
+    newCell.textContent = arr[i].missed_votes;
+    newCell = newRow.insertCell();
+    newCell.textContent = arr[i].missed_votes_pct.toFixed(2);
+  }
+}
+
+descTableTenPctAtt(
+  allSenateMembers,
+  document
+    .querySelector("#senate-least-attendance")
+    .getElementsByTagName("tbody")[0]
+);
+
+// Calculate most engaged (Top 10% Attendance) and build table
+
+function ascTableTenPctAtt(arr, table) {
+  arr.sort(function(obj1, obj2) {
+    return obj1.missed_votes_pct - obj2.missed_votes_pct;
+  });
+  for (i = 0; i < arr.length * 0.1; i++) {
+    let newRow = table.insertRow();
+    let newCell = newRow.insertCell();
+    let firstName = arr[i].first_name;
+    let middleName = arr[i].middle_name;
+    let lastName = arr[i].last_name;
+    if (middleName != null) {
+      fullName = firstName + " " + middleName + " " + lastName;
+    } else {
+      fullName = firstName + " " + lastName;
+    }
+    newCell.innerHTML = `<a href="${arr[i].url}" target="_blank">${fullName}</a>`;
+    newLink = newCell = newRow.insertCell();
+    newCell.textContent = arr[i].missed_votes;
+    newCell = newRow.insertCell();
+    newCell.textContent = arr[i].missed_votes_pct.toFixed(2);
+  }
+}
+
+ascTableTenPctAtt(
+  allSenateMembers,
+  document
+    .querySelector("#senate-most-attendance")
+    .getElementsByTagName("tbody")[0]
+);
